2cd286dced08f78ac0243fa136610baeff1279d4
Akshay Paunikar <akshayakki29@gmail.com>
2023-08-14 17:23:27 +0530

    To Do App created and tested successfully

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+    
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET','POST'])
 def hello_world():
-    return "Hello World ! Let's Learn Flask today !!!!"
+    if request.method=="POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        print(request.form['title'])
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    alltodo = Todo.query.all()
+    return render_template("index.html", alltodo=alltodo)
 
-@app.route('/products')
-def products():
-    return "this is products page"
+@app.route('/show')
+def show_data():
+    alltodo = Todo.query.all()
+    print(alltodo)
+    return "database created"
 
 if __name__ == "__main__":
     app.run(debug=True)
