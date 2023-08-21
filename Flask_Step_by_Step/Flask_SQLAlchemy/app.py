869d9b6e397bda0ebfb620d3a7fbab9d6d446b6d
Akshay Paunikar <akshayakki29@gmail.com>
2023-08-21 15:27:38 +0530

    Flask SQLAlchemy

--- Flask_Step_by_Step/Flask_SQLAlchemy/app.py
+++ Flask_Step_by_Step/Flask_SQLAlchemy/app.py
@@ -1,14 +1,19 @@
+# import libraries
 from flask import Flask, render_template, redirect, url_for, request, flash 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 
+# create extension
+db = SQLAlchemy()
+
+# create the app
 app = Flask(__name__)
 
-e = create_engine("sqlite:///students.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///students.db'
+# configure the SQLite database, relative to the app instance folder
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# initialize app with the extension
+db.init_app(app)
 
 class students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,35 +22,57 @@ class students(db.Model):
     city = db.Column(db.String(100), nullable=False)
     zipcode = db.Column(db.Integer, nullable=False)
     
-def __init__(self, name, gender, city, zipcode):
-    self.name = name
-    self.gender = gender
-    self.city = city
-    self.zipcode = zipcode
+with app.app_context():
+    db.create_all()
     
 @app.route('/')
 def home():
     return render_template('home.html', students=students.query.all())
 
-@app.route('/newStudent', methods=['GET','POST'])
-def new():
-    if request.method == 'POST':
-        if not request.form['name'] or not request.form['gender'] or not request.form['city'] or not request.form['zipcode']:
-            flash("Please check and enter data in all fields")
-        else:
-            name = request.form['name']
-            gender = request.form['gender']
-            city = request.form['city']
-            zipcode = int(request.form['zipcode'])
-            student = students(name=name, gender=gender, city=city, zipcode=zipcode)
-            
-            db.session.add(student)
-            db.session.commit()
-            flash("Record added successfully")
-            return redirect(url_for('home'))
+@app.route('/student')
+def info():
     return render_template('student.html')
 
+@app.route('/new', methods=['GET', 'POST'])
+def create_new():
+    if request.method == 'POST':
+        student = students(
+            name = request.form['name'],
+            gender = request.form['gender'],
+            city = request.form['city'],
+            zipcode = int(request.form['zipcode'])
+        )
+        db.session.add(student)
+        db.session.commit()
+        return redirect(url_for('home', id = student.id))
+    return render_template('student.html')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    if request.method=='POST':
+        name = request.form['name']
+        gender = request.form['gender']
+        city = request.form['city']
+        zipcode = int(request.form['zipcode'])
+        student = students.query.filter_by(id=id).first()
+        student.name = name
+        student.gender = gender
+        student.city = city
+        student.zipcode = zipcode
+        db.session.add(student)
+        db.session.commit()
+        return redirect("/")
+        
+    student = students.query.filter_by(id=id).first()
+    return render_template('update.html', student=student)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    student = students.query.filter_by(id=id).first()
+    db.session.delete(student)
+    db.session.commit()
+    return redirect("/")    
+
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True)
         
